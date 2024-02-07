fea39179191ae785b76902bcc4c28a535949a7b9
Appelg <50763504+Appelg@users.noreply.github.com>
2024-02-07 08:02:46 +0100

    initial commit

---
+++ KnxListener/Program.cs
@@ -0,0 +1,46 @@
+﻿using Knx.Bus.Common;
+using Knx.Bus.Common.Configuration;
+using Knx.Bus.Common.GroupValues;
+using Knx.Falcon.Sdk;
+
+namespace KnxListener
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var serverIp = args[0]; // 192.168.42.232
+            ushort port = 0x0e57;
+
+            Console.WriteLine($"Using serverip: {serverIp} : {port}");
+            using var bus = new Bus(new KnxIpTunnelingConnectorParameters(serverIp, 0x0e57, false));
+            bus.Connect();
+            bus.GroupValueReceived += args =>
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: GroupValueReceived: " + args.IndividualAddress + " Value: " + args.Value + " Address:" + args.Address);
+            };
+            bus.GroupValueReadReceived += args =>
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: GroupValueReadReceived: " + args.IndividualAddress + " Value: " + args.Value + " Address:" + args.Address);
+            };
+            bus.StateChanged += args =>
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss}: StateChanged: " + args);
+            }; ;
+
+            if (args.Length > 2)
+            {
+                var groupAddress = args[1];
+                var writeVal = int.Parse(args[2]);
+                Console.WriteLine($"Writing value {writeVal} to group address {groupAddress}");
+                bus.WriteValue(new GroupAddress("0/0/1"), new GroupValue(new TwoBit(writeVal)));
+            }
+
+            while (true)
+            {
+                await Task.Delay(5000);
+            }
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # KnxListener
+
+You need to install .net 8 runtime:
+https://dotnet.microsoft.com/en-us/download/dotnet/8.0
+
+## Just listen:
+```
+cd KnxListener
+dotnet run 192.168.42.232
+```
+
+## Also send a 1bit value to an group address (to trigger messages sent back on bus, e.g status of a device)
+```
+cd KnxListener
+dotnet run 192.168.42.232 0/0/1 1
+```
